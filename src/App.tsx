f0c847e74306e9d3366b5fabed9457a3b2a040c3
Mara Kim <marakim990@gmail.com>
2025-11-28 21:17:56 -0700

    setup webgpu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend, type ThreeToJSXElements } from "@react-three/fiber";
+
+import * as THREE from "three/webgpu";
+
+declare module "@react-three/fiber" {
+  interface ThreeElements extends ThreeToJSXElements<typeof THREE> {}
+}
+
+extend(THREE as any);
 
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas
+        gl={async (props) => {
+          const renderer = new THREE.WebGPURenderer(props as any);
+          await renderer.init();
+          return renderer;
+        }}
+      >
         <pointLight intensity={2} position={[0, 0, 4]} />
         <mesh rotation={[Math.PI / 4, Math.PI / 3, 0]} scale={2}>
           <boxGeometry />
